a600cdc9cda161c2dfc933c45fc510d3730d290d
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2023-06-05 16:32:52 +0700

    refactor: change model

---
+++ _script/script_for_face_detect.py
@@ -0,0 +1,14 @@
+import argparse
+import torch
+import requests
+from PIL import Image
+
+parser = argparse.ArgumentParser().add_argument('-s', '--source', type=str, help='Image url')
+source = parser.parse_args().source
+
+model = torch.hub.load("bryandlee/animegan2-pytorch:main", "generator", device='cpu').eval()
+face2paint = torch.hub.load("bryandlee/animegan2-pytorch:main", "face2paint", size=512)
+img = Image.open(requests.get(source, stream=True).raw).convert("RGB")
+out = face2paint(model, img)
+
+out.save('result.png')

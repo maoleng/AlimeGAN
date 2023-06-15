48beb22189b85547df0673466c3bd31b0e74ea2c
Bùi Hữu Lộc <hiepnguyen6014@gmail.com>
2023-06-15 16:51:37 +0700

    fix: res.next -> next

--- src/app/Http/Middlewares/MustLogin.js
+++ src/app/Http/Middlewares/MustLogin.js
@@ -1,14 +1,14 @@
 class MustLogin
 {
 
-    static handle(req, res)
+    static handle(req, res, next)
     {
         if (req.session.authed == null) {
             req.session.error = 'You must login first'
             return res.redirect('/')
         }
 
-        return res.next()
+        return next()
     }
 
 }
